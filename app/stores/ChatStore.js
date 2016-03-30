f85099a4059d2637881d820a065b17f30d00c269
Konrad Oleksiuk <konole@gmail.com>
2016-03-30 23:36:27 +0200

    Create DiscussionsDAO

--- app/stores/ChatStore.js
+++ app/stores/ChatStore.js
@@ -2,25 +2,46 @@ var Reflux = require('reflux');
 var ChatActions = require('../actions/ChatActions.js');
 var Discussion = require('../models/Discussion.js');
 
-var DISCUSSIONS = [
-  new Discussion({ id: 1, name: "Team channel", userIds: [1, 2]}),
-  new Discussion({ id: 2, name: "Fellow devs" , userIds: [1, 2]})
-];
+var DiscussionsDAO = require('../dao/DiscussionsDAO.js');
 
 var ChatStore = Reflux.createStore({
   listenables: [ChatActions],
 
+  discussions: [],
+  currentDiscussion: new Discussion(),
+  discussionsDao: null,
+
   onSwitchDiscussion: function(discussionId) {
     console.log("Chat store - onSwitchDiscussion:" + discussionId);
-    this.currentDiscussion = DISCUSSIONS.filter(function(discussion) { return discussion.id == discussionId})[0]; //TODO: Extract to DAO
+    this._fetchDiscussion(discussionId);
     this.trigger({ discussions: this.discussions, currentDiscussion: this.currentDiscussion });
   },
 
   getInitialState: function() {
-    this.discussions = DISCUSSIONS;
-    this.currentDiscussion = this.discussions[0];
+    if (!this.discussionsDao) {
+      this.discussionsDao = new DiscussionsDAO();
+      this._fetchDiscussions();
+    }
 
     return { discussions: this.discussions, currentDiscussion: this.currentDiscussion };
+  },
+
+  _fetchDiscussions: function() {
+    var onDiscussionsFetched = function(discussions) {
+      this.discussions = discussions;
+      this.trigger({ discussions: discussions, currentDiscussion: this.currentDiscussion });
+    }
+
+    this.discussionsDao.getDiscussions().then(onDiscussionsFetched.bind(this));
+  },
+
+  _fetchDiscussion: function(discussionId) {
+    var onDiscussionFetched = function(discussion) {
+      this.currentDiscussion = discussion;
+      this.trigger({ discussions: this.discussions, currentDiscussion: this.currentDiscussion });
+    }
+
+    this.discussionsDao.getDiscussion(discussionId).then(onDiscussionFetched.bind(this));
   }
 })
 
